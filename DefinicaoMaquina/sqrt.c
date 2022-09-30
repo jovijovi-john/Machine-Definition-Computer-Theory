7baa519d1abf8bd1b7bb39c6e9131ef970e5fead
John <jovijovi2019@hotmail.com>
2022-09-30 20:14:39 -0300

    sqrt

--- DefinicaoMaquina/sqrt.c
+++ DefinicaoMaquina/sqrt.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
+float square_root(float n);
+
 void main () {
-  printf("sqrt");
+  float n;
+  
+  printf("Enter the number: ");
+  scanf("%f", &n);
+  printf("\n");
+
+  printf("Square root is: %f", square_root(n));
 
   getchar();
   return;
+}
+
+// babylonian method
+float square_root(float n) {
+  float x = n;
+  float y = 1;
+  float a = 0.000001;
+
+  while ((x-y) > a) {
+    x = (x + y) / 2;
+    y = n / x;
+  }
+
+  return x;
 }
